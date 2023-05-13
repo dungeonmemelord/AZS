ca2c51cd02d26fa4707eb0e053b41fa4ef1abcde
Przemysław Szelenberger <xmspszelenberger@squarespace.com>
2023-05-13 22:39:35 +0200

    Add default options test

--- module/sheets/AZSActorSheet.test.js
+++ module/sheets/AZSActorSheet.test.js
@@ -4,4 +4,12 @@ describe('AZSActorSheet', () => {
   it('exists', () => {
     expect(AZSActorSheet).toBeDefined();
   });
+
+  it('gets default options', () => {
+    expect(AZSActorSheet.defaultOptions).toEqual({
+      classes: ['AZS', 'sheet', 'actor'],
+      width: 'auto',
+      height: 'auto',
+    });
+  });
 });
