28a02d539438cde08113d578d0d03f46203ce6e2
th3mon <p.szelenberger@gmail.com>
2023-08-03 05:35:38 +0200

    Dodaj link o CONFIG

--- module/record-configuration-values.js
+++ module/record-configuration-values.js
@@ -2,6 +2,7 @@ import { AZS } from './config.js';
 import { AZSActor } from './sheets/AZSActor.js';
 
 export const recordConfigurationValues = () => {
+  // INFO: https://foundryvtt.com/api/v10/modules/client.html#CONFIG
   CONFIG.AZS = AZS;
   CONFIG.Actor.entityClass = AZSActor;
   CONFIG.Actor.documentClass = AZSActor;
