a7ec6c91251c0df45c15ce7b9f5ea3472c1ed0a3
th3mon <p.szelenberger@gmail.com>
2023-08-03 03:58:44 +0200

    Przenieś funkcje registerActorSheets() i registerItemSheets()

--- module/sheets/register-sheets.js
+++ module/sheets/register-sheets.js
@@ -2,16 +2,16 @@ import AZSItemSheet from './AZSItemSheet.js';
 import AZSActorSheet from './AZSActorSheet.js';
 
 export const registerSheets = () => {
-  const registerActorSheets = () => {
-    Actors.unregisterSheet('core', ActorSheet);
-    Actors.registerSheet('AZS', AZSActorSheet, { makeDefault: true });
-  };
-
-  const registerItemSheets = () => {
-    Items.unregisterSheet('core', ItemSheet);
-    Items.registerSheet('AZS', AZSItemSheet, { makeDefault: true });
-  };
-
   registerActorSheets();
   registerItemSheets();
+};
+
+const registerActorSheets = () => {
+  Actors.unregisterSheet('core', ActorSheet);
+  Actors.registerSheet('AZS', AZSActorSheet, { makeDefault: true });
+};
+
+const registerItemSheets = () => {
+  Items.unregisterSheet('core', ItemSheet);
+  Items.registerSheet('AZS', AZSItemSheet, { makeDefault: true });
 };
